2d2f22cfab9180abb5ada3c6d4ff8c878521fba4
Danilo de Novais Silveira <coragi@gmail.com>
2020-08-18 11:25:08 -0300

    alteracao pra repo CP

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -14,7 +14,7 @@ export class MessageService {
 
   getRooms(parametro: number): Observable<Room> {
     var url: string;
-    url = "https://us-south.functions.appdomain.cloud/api/v1/web/82cc6e5f-1457-44aa-8b47-c5010c4e41e3/itau-devops/read-from-cloudant.json?id=" + parametro;
+    url = "https://us-south.functions.appdomain.cloud/api/v1/web/7ad9fcc4-9f77-43bc-a3cf-a1eb302381a5/ibmcloud/sequence-read-cloudant.json?id=" + parametro;
     console.log("url", url);
     return this.http.get<Room>(url)
       .pipe(
